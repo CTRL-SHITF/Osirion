119b5b39fac6eea578b54dd831e618b0e0626ab8
CTRL-SHITF <106828119+CTRL-SHITF@users.noreply.github.com>
2025-04-30 16:18:33 -0400

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,8 +1,8 @@
  document.addEventListener("DOMContentLoaded", function () {
     const descriptions = [
-        { title: "FODDER", description: "What is this game?", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quo nam veritatis labore, distinctio quasi quidem similique. Nihil voluptatum veritatis praesentium, deserunt vitae culpa! Modi ut ipsa obcaecati sit quaerat.." },
-        { title: "ADVENTURE", description: "An epic journey awaits!", text: "Explore vast lands, defeat monsters, and become a hero." },
-        { title: "MYSTIC", description: "Unleash your power!", text: "Harness magical abilities and fight against darkness." }
+        { title: "FODDER", description: "What is this game?", text: "In FODDER, disposable agents face a ruthless world.Every choice matters, every life is fragile. Plan your heists, survive the fallout, and uncover the truth behind the syndicates’ war..." },
+        { title: "ADVENTURE", description: "An epic journey awaits!", text: "Embark on an adventurous journey through enchanted forests and mountains, using navigation skills to uncover secrets and face challenging enemies. Equip varied weapons, including ancient swords and magical artifacts." },
+        { title: "MYSTIC", description: "Unleash your power!", text: "Embark on an adventurous journey through enchanted forests and mountains, using navigation skills to uncover secrets and face challenging enemies. Equip varied weapons, including ancient swords and magical artifacts." }
     ];
 
     const radioButtons = document.querySelectorAll('input[name="slider"]');
@@ -14,6 +14,7 @@
         radio.addEventListener("change", function () {
             titleElement.textContent = descriptions[index].title;
             descElement.textContent = descriptions[index].description;
+            textElement.textContent = descriptions[index].text;
         });
     });
 });
